fcb9077e2d169ca9932deb83bf8359613a7283de
Philip Borenstein <philip.borenstein@docusign.com>
2017-02-10 22:47:54 -0800

    Explain how things work

--- analyze_post_by_count.js
+++ analyze_post_by_count.js
@@ -6,8 +6,31 @@ var posts = require('./posts.json');
 var _ = require('lodash');
 var moment = require('moment');
 
-console.log(_.chain(posts).sortBy(function (post) {
-  return post.timestamp;
-}).value().reverse().map(function (post) {
-  return [post.note_count, post.post_url, moment(post.timestamp * 1000).format('YYYY-MM-DD')].join(';');
-}).join('\n'));
+function getPostTimestamp(posts) {
+  return post.timestamp
+}
+
+//  For the given post,
+//  Return a string of the form;
+//  66;http://thedailytask.tumblr.com/post/109879218750/150202;2015-02-02
+
+//  from three-element array:
+//    [note_count, post_url, date]
+//  
+//  join this into a semicolon-separated string
+
+function getCountDate (post) {
+  return [post.note_count,
+          post.post_url,
+          moment(post.timestamp * 1000).format('YYYY-MM-DD')
+         ]
+         .join(';');
+}
+
+console.log(
+  _.chain(posts)              // for every post
+   .sortBy(getPostTimestamp)  // sorted by post timestamp
+   .value()                   // turn it into an array
+   .reverse()                 // reverse it
+   .map(getCountDate)         // collect the count/date
+   .join('\n'));              // Join each line with a newline
