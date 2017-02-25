e8a063664cca2923e3647223fc3af2a6eac02432
Philip Borenstein <pborenstein@gmail.com>
2017-02-25 00:08:03 -0500

    Explain how we get credentials

--- retrieve_posts.js
+++ retrieve_posts.js
@@ -11,83 +11,71 @@ var fs        = require('fs');
 var JSON5     = require('json5');
 
 // Load credentials
-var credentials = (function() {
-    var credsFile = argv.credentials || _.find([
-        'tumblr-credentials.json',
-        'credentials.json',
-        path.join(osHomedir(), 'tumblr-credentials.json'),
-    ], function(credsFile) {
-        return fs.existsSync(credsFile);
-    }) || 'credentials.json';
+function getCredentials(argCreds) {
 
-    try {
-        var credentials = JSON5.parse(fs.readFileSync(credsFile).toString());
-    } catch (e) {
-        console.error(chalk.red('Error loading credentials!'));
-        console.error('Make sure %s exists or specify %s', chalk.cyan(credsFile || 'credentials.json'), chalk.cyan('--credentials=path/to/credentials.json'));
-        process.exit();
+        // Is there a filename in args?
+
+  var credsFile = argCreds ||
+
+        // no? Use the first file that exists:
+        //    * ./tumblr-credentials.json
+        //    * ./credentials.json
+        //    * ~/tumblr-credentials.json
+
+                  _.find(['tumblr-credentials.json',
+                          'credentials.json',
+                          path.join(osHomedir(), 'tumblr-credentials.json')
+                        ],
+                        credsFile => fs.existsSync(credsFile)) ||
+
+        // still no match? Use credentials.json
+
+                  'credentials.json';
+
+        //  read and parse the credsFile
+
+  try {
+    var credentials = JSON5.parse(fs.readFileSync(credsFile).toString());
+  } catch (e) {
+    console.error(chalk.red('Error loading credentials!'));
+    console.error('Make sure %s exists or specify %s', chalk.cyan(credsFile || 'credentials.json'), chalk.cyan('--credentials=path/to/credentials.json'));
+    process.exit();
+  }
+
+  console.log('\nUsing OAuth creds from %s\n', chalk.magenta(path.resolve(credsFile)));
+
+        //  do we have all the properties we need?
+        //  1. get the missing keys into missingCredentials
+
+  var missingKeys = _.remove([  'consumer_key',
+                                'consumer_secret',
+                                'token',
+                                'token_secret'  ],
+                              cred => !credentials.hasOwnProperty(cred))
+
+        // 2. Print the missing keys
+
+  if (!_.isEmpty(missingKeys)) {
+    console.warn(chalk.yellow('Credentials is missing keys:'));
+    missingKeys.forEach(key => console.warn(chalk.yellow('  * %s'), key));
+
+        // 3. Say how to generate user tokens if possible
+
+    if (credentials.consumer_key && credentials.consumer_secret) {
+      console.log('\nYou can generate user tokens by going to the API console:\n')
+      console.log(chalk.magenta('https://api.tumblr.com/console/auth' +
+                                `?consumer_key=${credentials.consumer_key}` +
+                                `&consumer_secret=${credentials.consumer_secret}`));
     }
+  }
 
-    // console.log('\nUsing OAuth creds from %s\n', chalk.magenta(path.resolve(credsFile)));
-
-//     var missingCredentials = 
-//           _.remove(['consumer_key', 'consumer_secret', 'token', 'token_secret'],
-//                     _.partial(_.negate(_.has), credentials));
-// 
-// could do
-//    ['consumer_key', 'consdumer_secret', 'token', 'token_secret']
-//      .reduce(function(acc,x){return acc && credentials.hasOwnProperty(x)}, true)
-// but that won't tell you what's missing
-//
-// but see: https://www.sitepoint.com/lodash-features-replace-es6/#5partial
-//  var sayHelloTo = _.partial(greet, 'hello');
-//  const sayHelloTo = name => greet('hello', name);
-//
-// so we can replace:
-//    _.partial(_.negate(_.has), credentials)
-// with this:
-//    cred => !credentials.hasOwnProperty(cred)
-//
-//  So the missing credentials:
-//    _.remove(['consumer_key', 'consumer_secret', 'token', 'token_secret'],
-//                   cred => !credentials.hasOwnProperty(cred))
-//
-//    Remove from the list any element
-//      that is not a property of the credentials object
-//      return the removed elements
-//    Which is really kind of a counterintuitive way of doing this.
-//    what you want to say is 
-//      here' a list of keys
-//      tell me which ones aren't in the credential object
-//      This says --- ugh it makes my brain hurt
-
-    var missingCredentials = 
-          _.remove(['consumer_key', 'consumer_secret', 'token', 'token_secret'],
-                   cred => !credentials.hasOwnProperty(cred))
-
-    if (!_.isEmpty(missingCredentials)) {
-        console.warn(chalk.yellow('Credentials is missing keys:'));
-        missingCredentials.forEach(function(key) {
-            console.warn(chalk.yellow('  * %s'), key);
-        });
-
-        if (credentials.consumer_key && credentials.consumer_secret) {
-            console.log('\nYou can generate user tokens by going to the API console:\n');
-            console.log(chalk.magenta([
-                'https://api.tumblr.com/console/auth',
-                '?consumer_key=', credentials.consumer_key,
-                '&consumer_secret=', credentials.consumer_secret,
-            ].join('')));
-        }
-    }
-
-    return credentials;
-})();
+  return credentials;
+}
 
 
+var client = tumblr.createClient(getCredentials(argv.credentials));
 
 
-var client = tumblr.createClient(credentials);
 
 // quick & dirty it is, life's short, done is better than perfect, I only had 2 hours for the whole project.
 // boy, I need to go past this, my eyes are bleeding...
