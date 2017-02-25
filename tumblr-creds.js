ba7977347bb3221365a0c32b0129592647408428
Philip Borenstein <pborenstein@gmail.com>
2017-02-24 22:11:48 -0500

    Can we make formatting clearer?

--- tumblr-creds.js
+++ tumblr-creds.js
@@ -1,19 +1,24 @@
 function getCredentials(argCreds) {
-    // Is there a filename in args?
-  var credsFile = argCreds ||
-    // no? Use the first file that exists:
-    //    * ./tumblr-credentials.json
-    //    * ./credentials.json
-    //    * ~/tumblr-credentials.json
-    _.find(['tumblr-credentials.json',
-      'credentials.json',
-      path.join(osHomedir(), 'tumblr-credentials.json')],
-      credsFile => fs.existsSync(credsFile)) ||
-    // still no match? Use credentials.json
-    'credentials.json';
+        // Is there a filename in args?
+
+  var credsFile = argCreds
+
+        // no? Use the first file that exists:
+        //    * ./tumblr-credentials.json
+        //    * ./credentials.json
+        //    * ~/tumblr-credentials.json
+
+    || _.find(['tumblr-credentials.json',
+            'credentials.json',
+            path.join(osHomedir(), 'tumblr-credentials.json')
+           ],
+           credsFile => fs.existsSync(credsFile))
+
+        // still no match? Use credentials.json
+
+    || 'credentials.json';
 
   //  try reading & parsing the file
-
   try {
     var credentials = JSON5.parse(fs.readFileSync(credsFile).toString());
   } catch (e) {
@@ -22,24 +27,23 @@ function getCredentials(argCreds) {
     process.exit();
   }
 
+      //  do we have all the properties we need?
+      //  get the missingCredentials
 
   var missingCredentials =
-    _.remove(['consumer_key', 'consumer_secret', 'token', 'token_secret'],
-      cred => !credentials.hasOwnProperty(cred))
+      _.remove(['consumer_key', 'consumer_secret', 'token', 'token_secret'],
+               cred => !credentials.hasOwnProperty(cred))
 
   if (!_.isEmpty(missingCredentials)) {
     console.warn(chalk.yellow('Credentials is missing keys:'));
-    missingCredentials.forEach(function (key) {
-      console.warn(chalk.yellow('  * %s'), key);
-    });
+    missingCredentials.forEach(key => console.warn(chalk.yellow('  * %s'), key));
 
     if (credentials.consumer_key && credentials.consumer_secret) {
       console.log('\nYou can generate user tokens by going to the API console:\n');
-      console.log(chalk.magenta([
-        'https://api.tumblr.com/console/auth',
-        '?consumer_key=', credentials.consumer_key,
-        '&consumer_secret=', credentials.consumer_secret,
-      ].join('')));
+      console.log(chalk.magenta([ 'https://api.tumblr.com/console/auth',
+                                  '?consumer_key=', credentials.consumer_key,
+                                  '&consumer_secret=', credentials.consumer_secret
+                                ].join('')));
     }
   }
 
